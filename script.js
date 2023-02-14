6075aa946e78b0f87237143b525da0ccdc1ccd00
RChipman <rschipman@gmail.com>
2023-02-14 19:54:40 -0300

    more stuff n things

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const btn = document.querySelector("#btn");
-btn.addEventListener("click", function () {
+const heading = document.querySelector("#heading");
+
+const makeRandColor = () => {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
-  const newColor = `rgb(${r},${g},${b})`;
+  return `rgb(${r},${g},${b})`;
+};
+
+btn.addEventListener("click", function () {
+  const newColor = makeRandColor();
   document.body.style.backgroundColor = newColor;
+  heading.innerText = newColor;
 });
